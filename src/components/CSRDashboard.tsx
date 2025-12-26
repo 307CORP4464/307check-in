1dad5fc758a230288da890173c592fd1702d7f39
307CORP4464 <307corp@gmail.com>
2025-12-26 12:23:24 -0500

    Update CSRDashboard.tsx

--- src/components/CSRDashboard.tsx
+++ src/components/CSRDashboard.tsx
@@ -487,11 +487,4 @@ export default function CSRDashboard() {
     </div>
   );
 }
-        <div className="bg-white rounded-lg shadow p-6">
-          <h2 className="text-xl font-semibold mb-4">Check-ins</h2>
-          <p className="text-gray-500">Table content will go here...</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+
