bec3aeb96be9211d2679c6ac23788cbeb2741917
307CORP4464 <307corp@gmail.com>
2026-01-02 15:13:56 -0500

    Update page.tsx

--- src/app/dock-status/page.tsx
+++ src/app/dock-status/page.tsx
@@ -139,7 +139,8 @@ export default function DockStatusPage() {
         
         dock.orders = orders;
         if (orders.length > 0) {
-          dock.current_load_id = orders<a href="" class="citation-link" target="_blank" style="vertical-align: super; font-size: 0.8em; margin-left: 3px;">[0]</a>.id;
+         dock.current_load_id = orders<a href="" class="citation-link" target="_blank" style="vertical-align: super; font-size: 0.8em; margin-left: 3px;">[0]</a>.id;
+
         }
       });
 
