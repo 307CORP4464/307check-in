f74b86c10ca7d4e9e08ea1eb09fb6b64324b98d7
307CORP4464 <307corp@gmail.com>
2026-01-19 15:36:43 -0500

    Update AssignDockModal.tsx

--- src/components/AssignDockModal.tsx
+++ src/components/AssignDockModal.tsx
@@ -661,7 +661,7 @@ export default function AssignDockModal({ checkIn, onClose, onSuccess, isOpen }:
             <tr><td>TRAILER HAS BEEN LATCH PROPERLY?</td><td class="checkbox-cell">☐</td></tr>
             <tr><td>THE TRAILER BEEN SEALED?</td><td class="checkbox-cell">☐</td></tr>
             <tr><td>CUSTOMER REQUIRED PHOTOS TAKEN AND SENT?</td><td class="checkbox-cell">☐</td></tr>
-            <tr><td></td>INITIAL _______</td></tr>
+            <tr><td>INITIALS OF PERSON THAT SEALED</td><td class="checkbox-cell">__________</td></tr>
             
           </tbody>
         </table>
