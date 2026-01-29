0eb17f047d960cb156bdd81ffb16fb9154897d7b
307CORP4464 <307corp@gmail.com>
2026-01-29 10:52:09 -0500

    Update page.tsx

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -54,7 +54,7 @@ export default function AppointmentsPage() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')<a href="" class="citation-link" target="_blank" style="vertical-align: super; font-size: 0.8em; margin-left: 3px;">[0]</a>);
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
   const [appointments, setAppointments] = useState<Appointment[]>([]);
   const [loading, setLoading] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
@@ -85,7 +85,7 @@ export default function AppointmentsPage() {
       console.log('📊 Total count:', data.length);
       
       if (data.length > 0) {
-        console.log('🔍 First appointment:', data<a href="" class="citation-link" target="_blank" style="vertical-align: super; font-size: 0.8em; margin-left: 3px;">[0]</a>);
+        console.log('🔍 First appointment:', data[0]);
         console.log('🕐 Appointment times:', data.map(a => a.appointment_time));
       }
       
@@ -101,7 +101,7 @@ export default function AppointmentsPage() {
   const changeDateByDays = (days: number) => {
     const currentDate = new Date(selectedDate);
     currentDate.setDate(currentDate.getDate() + days);
-    setSelectedDate(currentDate.toISOString().split('T')<a href="" class="citation-link" target="_blank" style="vertical-align: super; font-size: 0.8em; margin-left: 3px;">[0]</a>);
+    setSelectedDate(currentDate.toISOString().split('T')[0]);
   };
 
   const filteredAppointments = appointments.filter(apt => {
