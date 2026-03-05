d3e479cb5fded04cef14f7b0d58ff793a057f9cb
307CORP4464 <307corp@gmail.com>
2026-03-05 14:07:30 -0500

    Update EditCheckInModal.tsx

--- src/components/EditCheckInModal.tsx
+++ src/components/EditCheckInModal.tsx
@@ -316,7 +316,7 @@ export default function EditCheckInModal({ checkIn, onClose, onSuccess, isOpen }
                 className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500"
               >
                 <option value="">Select...</option>
-                <option value="Box">Box Truck</option>
+                <option value="Box/Van">Box Truck or Van</option>
                 <option value="20">20'</option>
                 <option value="40">40'</option>
                 <option value="45">45'</option>
