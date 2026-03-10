f73de3a945fe8e029b0804fc51df5fffb9be2a6c
307CORP4464 <307corp@gmail.com>
2026-03-10 14:32:30 -0400

    Update DriverCheckInForm.tsx

--- src/components/DriverCheckInForm.tsx
+++ src/components/DriverCheckInForm.tsx
@@ -43,7 +43,7 @@ const US_STATES = [
   'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
   'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
   'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-  'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'Mexico', 'Canada'
+  'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'MX', 'CA'
 ] as const;
 
 const TRAILER_LENGTHS = [
