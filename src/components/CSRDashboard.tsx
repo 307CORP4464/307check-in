ab680d166ab2abde7caaba2831edbb50a5edfffc
307CORP4464 <307corp@gmail.com>
2026-03-14 14:42:07 -0400

    Update CSRDashboard.tsx

--- src/components/CSRDashboard.tsx
+++ src/components/CSRDashboard.tsx
@@ -625,7 +625,7 @@ export default function CSRDashboard() {
 
                       {/* Transport */}
                       <td className="px-4 py-3 text-sm">
-                        {checkIn.carrier && checkIn.smode
+                        {checkIn.carrier && checkIn.mode
                           ? `${checkIn.carrier}, ${checkIn.mode}`
                           : 'N/A'}
                       </td>
