babb5d7ba4ac3a303172435cd192e2f23a89d974
307CORP4464 <307corp@gmail.com>
2026-04-15 06:23:25 -0400

    Update DriverStatusLookup.tsx

--- src/components/DriverStatusLookup.tsx
+++ src/components/DriverStatusLookup.tsx
@@ -80,7 +80,7 @@ const getStatusMeta = (status: string): StatusMeta => {
       return {
         headerBg: 'bg-amber-500', headerTitle: 'Submitted - Pending Dock Assignment', headerIcon: '',
         bannerBg: 'bg-amber-50', bannerBorder: 'border-amber-300', bannerText: 'text-amber-700',
-        bannerIcon: <Clock className="w-5 h-5 text-amber-500" />, bannerLabel: 'We are processing your check-in, please note this may take several minutes.',
+        bannerIcon: <Clock className="w-5 h-5 text-amber-500" />, bannerLabel: 'We are processing your check-in, please note this may take several minutes.Please wait in your truck for this page to update.',
       };
     case 'checked_in':
       return {
